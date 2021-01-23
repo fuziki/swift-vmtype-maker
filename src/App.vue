28a491dbee5acf0c00e25750fd3d72c82041f836
fuziki <hymne.shannon.projects@gmail.com>
2021-01-24 02:59:43 +0900

    router.js を追加

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/icon_100.png">
-    <HelloWorld/>   
-    <footer>
-      <Footer/>
-    </footer>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    Footer,
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
